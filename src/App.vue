77e0d5e674ed2fa46e90e03dded9405240dd4625
ThaphatTT <thaphat.meechaitana@outlook.com>
2024-08-06 14:49:06 +0700

    import file & create router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import Signin from './views/Signin.vue';
 import { RouterView } from 'vue-router';
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,20 @@ import {createRouter, createWebHistory} from 'vue-router'
 
 import Signin from '@/views/Signin.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import Signup from '@/views/Signup.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes:[
     {
-      path: '/signup',
+      path: '/signin',
       name: 'sign-in',
       component:Signin
+    },
+    {
+      path: '/signup',
+      name: 'sign-up',
+      component:Signup
     },
     {
       path: '/:catchAll(.*)',
